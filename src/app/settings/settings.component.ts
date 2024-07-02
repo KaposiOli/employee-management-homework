39c5ad49b4b50432280a354c3d8286be4476b7bd
KaposiOli <oliver.kaposi@gmail.com>
2024-07-02 14:52:49 +0200

    feat(settings): settings component and token save to localStorage

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrl: './settings.component.scss',
 })
 export class SettingsComponent {
+  public authToken: string = '';
 
+  saveAuthData() {
+    localStorage.setItem('authToken', this.authToken);
+  }
 }
