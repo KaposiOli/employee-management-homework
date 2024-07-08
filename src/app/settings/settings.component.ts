b2ff3b22fca1fe37f0e7267b897b6014707b025f
KaposiOli <oliver.kaposi@gmail.com>
2024-07-08 20:19:28 +0200

    feat(settings): settings component update

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AuthService, AuthToken } from '../services/auth/auth.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatCardModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss',
 })
-export class SettingsComponent {
-  public authToken: string = '';
+export class SettingsComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  public tokenFromApi: string = '';
+
+  ngOnInit(): void {
+    this.authService.getAuthToken().subscribe({
+      next: (data: AuthToken) => {
+        this.tokenFromApi = data.access_token;
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+    });
+  }
 
   saveAuthData() {
-    localStorage.setItem('authToken', this.authToken);
+    localStorage.setItem('authToken', this.tokenFromApi);
   }
 }
